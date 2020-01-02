b015ea67920057d356d7182bfaaca5a63c0e896f
eishi <saito@eishis.com>
2020-01-02 13:05:58 +0900

    remove sass load

--- lib/yukiusagi.rb
+++ lib/yukiusagi.rb
@@ -2,7 +2,7 @@ require "yukiusagi/version"
 require 'sassc-rails'
 
 module Yukiusagi
-  Sass.load_paths << File.expand_path("../app/assets/stylesheets", __FILE__)
+  # Sass.load_paths << File.expand_path("../app/assets/stylesheets", __FILE__)
   if defined?(Rails)
     class Engine < Rails::Engine
     end
